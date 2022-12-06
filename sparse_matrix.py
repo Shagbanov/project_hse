e15081dc4c1ff436a2be4f67f475505a679734a8
MrDoberman <ahmedshagbanov70@gmail.com>
2022-12-06 14:27:47 +0300

    Initial commit

---
+++ sparse_matrix.py
@@ -0,0 +1,367 @@
+import copy
+import bisect
+
+
+class Matrix:
+    """Класс матрица:
+       1) Операторы: +(++) -(++) *(++) **-1(++) ~(++) *n(++) [](++)
+       2)Удаление(++) и перестановка строк и столбцов(++), получение ранга матрицы(++),
+        получение размерности(++), преобразование к списку(++), транспонирование(++)
+       3) Иерархия исключений
+       """
+
+    def __init__(self, matrix):
+        """
+            Создание матрицы
+            Можно ввести как список списков и также как сжатое хранение строкой
+        """
+        if type(matrix[-1]) == int:
+            self.m = matrix
+            return
+        row_index = [0]
+        column_index = []
+        values = []
+        count = 0
+        for i in range(len(matrix)):
+            if len(matrix[i]) != len(matrix[0]):
+                raise ValueError("Разное количество элементов в строках")
+            for j in range(len(matrix[0])):
+                if matrix[i][j] != 0:
+                    count += 1
+                    column_index.append(j)
+                    values.append(matrix[i][j])
+            row_index.append(count)
+        self.m = [row_index, column_index, values, len(matrix[0])]
+
+    def matrix(self):
+        """
+        Возвращает матрицу в виде сжатого хранения строкой
+        :return: матрицу(сжатое хранение строкой)
+        """
+        return self.m
+
+    def to_matrix(self):
+        """
+        Возвращает матрицу в виде списка списков
+        :return: матрицу(список списков)
+        """
+        matrix = self.m
+        matrix_tabl = [[0] * matrix[3] for i in range(len(matrix[0]) - 1)]
+        for i in range(1, len(matrix[0])):
+            for j in range(matrix[0][i] - matrix[0][i - 1]):
+                matrix_tabl[i - 1][matrix[1][matrix[0][i - 1]:matrix[0][i]][j]] = \
+                    matrix[2][matrix[0][i - 1]:matrix[0][i]][j]
+        return matrix_tabl
+
+    def __add__(self, other):
+        """
+        Сложение двух матриц
+        :param other: вторая матрица
+        :return: возвращает новую матрицу, полученную в результате суммы двух матриц
+        """
+        matrix = copy.deepcopy(self.m)
+        matrix_o = copy.deepcopy(other.m)
+        if len(matrix[0]) != len(matrix_o[0]) or matrix[3] != matrix_o[3]:
+            raise IndexError("Матрицы разных размеров")
+        for i in range(1, len(matrix[0])):
+            count = 0
+            column1 = matrix[1][matrix[0][i - 1]:matrix[0][i]]
+            column2 = matrix_o[1][matrix_o[0][i - 1]:matrix_o[0][i]]
+
+            for j in range(len(column2)):
+                if column2[j] not in column1:
+                    ind = bisect.bisect(column1, column2[j])
+                    count += 1
+                    column1.insert(ind, column2[j])
+                    matrix[2].insert(ind + matrix[0][i - 1], matrix_o[2][matrix_o[0][i - 1]:matrix_o[0][i]][j])
+                else:
+                    matrix[2][column1.index(column2[j]) + matrix[0][i - 1]] += matrix_o[2][j + matrix_o[0][i - 1]]
+            matrix[1][matrix[0][i - 1]:matrix[0][i]] = column1
+            for k in range(i, len(matrix[0])):
+                matrix[0][k] += count
+        return Matrix(matrix)
+
+    def __sub__(self, other):
+        """
+        Разность двух матриц
+        :param other: вторая матрица
+        :return: возвращает новую матрицу, полученную в результате разности двух матриц
+        """
+        matrix = copy.deepcopy(self.m)
+        matrix_o = copy.deepcopy(other.m)
+        if len(matrix[0]) != len(matrix_o[0]) or matrix[3] != matrix_o[3]:
+            raise IndexError("Матрицы разных размеров")
+        for i in range(1, len(matrix[0])):
+            count = 0
+            column1 = matrix[1][matrix[0][i - 1]:matrix[0][i]]
+            column2 = matrix_o[1][matrix_o[0][i - 1]:matrix_o[0][i]]
+
+            for j in range(len(column2)):
+                if column2[j] not in column1:
+                    ind = bisect.bisect(column1, column2[j])
+                    count += 1
+                    column1.insert(ind, column2[j])
+                    matrix[2].insert(ind + matrix[0][i - 1], 0 - matrix_o[2][matrix_o[0][i - 1]:matrix_o[0][i]][j])
+                else:
+                    matrix[2][column1.index(column2[j]) + matrix[0][i - 1]] -= matrix_o[2][j + matrix_o[0][i - 1]]
+            matrix[1][matrix[0][i - 1]:matrix[0][i]] = column1
+            for k in range(i, len(matrix[0])):
+                matrix[0][k] += count
+        return Matrix(matrix)
+
+    def __mul__(self, other):
+        """
+        Произведение двух матриц или умножение матрица на число
+        :param other: вторая матрица или число
+        :return: возвращает новую матрицу, полученную в результате произведения двух матриц или матрица на число
+        """
+        matrix = copy.deepcopy(self.m)
+        if type(other) == int or type(other) == float:
+            for i in range(len(matrix[2])):
+                matrix[2][i] *= other
+            matrix_answ = matrix
+        else:
+            matrix2 = copy.deepcopy(other.transposition().m)
+            if matrix[3] != len(matrix2[0])-1:
+                raise IndexError("Количество столбцов не равно количеству строк")
+            matrix_answ = [[0] * len(matrix[0]), [], [], matrix2[-1]]
+            for i in range(1, len(matrix[0])):
+                matrix_answ[0][i] += matrix_answ[0][i - 1]
+
+                column = 0
+                column1 = matrix[1][matrix[0][i - 1]:matrix[0][i]]
+                values1 = matrix[2][matrix[0][i - 1]:matrix[0][i]]
+
+                for j in range(1, len(matrix2[0])):
+                    count = 0
+                    column2 = matrix2[1][matrix2[0][j - 1]:matrix2[0][j]]
+                    values2 = matrix2[2][matrix2[0][j - 1]:matrix2[0][j]]
+
+                    for k in range(len(column1)):
+                        if column1[k] in column2:
+                            count += values1[k] * values2[column2.index(column1[k])]
+
+                    if count != 0:
+                        matrix_answ[0][i] += 1
+                        matrix_answ[1].append(column)
+                        matrix_answ[2].append(count)
+
+                    column += 1
+
+        return Matrix(matrix_answ)
+
+    def __getitem__(self, item):
+        """
+        Получение строки или столбца номером item(matrix[:][0] - первый столбец)
+        :param item: номер столбца или строки
+        :return: список
+        """
+        matrix = copy.deepcopy(Matrix(self.m))
+        if isinstance(item, slice) and item == slice(None, None, None):
+            matrix = matrix.transposition()
+        else:
+            if not isinstance(item, int):
+                raise TypeError("Индекс должен быть целым числом")
+            if item > len(matrix.matrix()[0]) - 2:
+                raise IndexError("Индекс все списка")
+        return matrix.to_matrix()[item]
+
+    def transposition(self):
+        """
+        Транспонирование матрицы
+        :return: новую матрицу
+        """
+        matrix = copy.deepcopy(self.m)
+        matrix_answ = [[0] * (matrix[3] + 1), [], [], len(matrix[0])-1]
+        for count in range(matrix[3]):
+            matrix_answ[0][count + 1] += matrix_answ[0][count]
+            for j in range(len(matrix[1])):
+                if matrix[1][j] == count:
+                    matrix_answ[0][count + 1] += 1
+                    matrix_answ[1].append(bisect.bisect(matrix[0], j) - 1)
+                    matrix_answ[2].append(matrix[2][j])
+        return Matrix(matrix_answ)
+
+    def delete(self, line, column):
+        """
+        Удаляет строку номером line и столбец номером column
+        :param line: номер строки
+        :param column: номер столбца
+        :return: новая матрица
+        """
+        matrix = copy.deepcopy(self.m)
+        if line > len(matrix[0]) - 1:
+            raise IndexError("Индекс вне списка(строка)")
+        elif column > matrix[3]:
+            raise IndexError("Индекс вне списка(столбец)")
+        del matrix[1][matrix[0][line - 1]:matrix[0][line]]
+        del matrix[2][matrix[0][line - 1]:matrix[0][line]]
+        count = matrix[0][line] - matrix[0][line - 1]
+        for i in range(line + 1, len(matrix[0])):
+            matrix[0][i] -= count
+        del matrix[0][line]
+
+        columns_arr = []
+        for i in range(1, len(matrix[0])):
+            columns_arr.append(matrix[1][matrix[0][i - 1]:matrix[0][i]])
+
+        for i in range(len(columns_arr)):
+            if (column - 1) in columns_arr[i]:
+                for j in range(columns_arr[i].index(column - 1) + 1, len(columns_arr[i])):
+                    matrix[1][j + matrix[0][i]] -= 1
+                for j in range(i + 1, len(matrix[0])):
+                    matrix[0][j] -= 1
+
+                del matrix[1][matrix[0][i] + columns_arr[i].index(column - 1)]
+                del matrix[2][matrix[0][i] + columns_arr[i].index(column - 1)]
+                continue
+            else:
+                for j in range(len(columns_arr[i])):
+                    if columns_arr[i][j] > column - 1:
+                        matrix[1][j + matrix[0][i]] -= 1
+        matrix[3] -= 1
+        return Matrix(matrix)
+
+    def __invert__(self):
+        """
+        Определитель матрицы (~matrix)
+        :return: число
+        """
+        count = 0
+        matrix = copy.deepcopy(self.m)
+        if len(matrix[0]) - 1 != matrix[3]:
+            raise IndexError("Не квадратная матрица")
+        if len(matrix[0]) == 2 and len(matrix[2]) > 0:
+            return matrix[2][0]
+        elif len(matrix[2]) == 0:
+            return 0
+        else:
+            for i in range(matrix[3]):
+                m = Matrix(matrix).delete(1, i + 1)
+                if i in matrix[1][matrix[0][0]:matrix[0][1]]:
+                    count += matrix[2][matrix[1][matrix[0][0]:matrix[0][1]].index(i)] * ~m * (-1) ** (i + 2)
+        return count
+
+    def size(self):
+        """
+        Размер матрицы
+        :return: строка
+        """
+        return f"{len(self.m[0]) - 1} на {self.m[3]}"
+
+    def permutation(self, line_column: str, number_i, number_j):
+        """
+        Перестановка строк и столбцов (matrix.permutation("строка", 1, 2) - переставляем 1 строку и 2 строку)
+        :param line_column: что переставляем: строку или столбец
+        :param number_i: номер первой строки или первого столбца
+        :param number_j: номер второй строки или второго столбца
+        :return: новую матрицу
+        """
+        line_column = line_column.lower()
+        matrix = copy.deepcopy(self.m)
+        if line_column == "строка" and (number_i > len(matrix[0]) -1 or number_j > len(matrix[0])-1):
+            raise IndexError("Индекс вне списка(строка)")
+        elif line_column == "столбец" and (number_i > matrix[3] or number_j > matrix[3]):
+            raise IndexError("Индекс вне списка(столбец)")
+        if number_i > number_j:
+            number_i, number_j = number_j, number_i
+
+        if line_column == "строка":
+            matrix[1][matrix[0][number_j - 1]:matrix[0][number_j]], matrix[1][matrix[0][number_i - 1]:matrix[0][number_i]] = matrix[1][matrix[0][number_i - 1]:matrix[0][number_i]], matrix[1][matrix[0][number_j - 1]:matrix[0][number_j]]
+            matrix[2][matrix[0][number_j - 1]:matrix[0][number_j]], matrix[2][matrix[0][number_i - 1]:matrix[0][number_i]] = matrix[2][matrix[0][number_i - 1]:matrix[0][number_i]], matrix[2][matrix[0][number_j - 1]:matrix[0][number_j]]
+            count_i = matrix[0][number_i] - matrix[0][number_i - 1]
+            count_j = matrix[0][number_j] - matrix[0][number_j - 1]
+            matrix[0][number_i] = matrix[0][number_i - 1] + count_j
+            for i in range(number_i + 1, number_j):
+                matrix[0][i] += count_j - count_i
+            matrix[0][number_j] = matrix[0][number_j - 1] + count_i
+        elif line_column == "столбец":
+            number_i -= 1
+            number_j -= 1
+            for i in range(1, len(matrix[0])):
+                if number_i in matrix[1][matrix[0][i - 1]:matrix[0][i]] and number_j in matrix[1][matrix[0][i - 1]:matrix[0][i]]:
+                    t = matrix[2][matrix[0][i - 1]:matrix[0][i]]
+                    t[matrix[1][matrix[0][i - 1]:matrix[0][i]].index(number_i)], t[matrix[1][matrix[0][i - 1]:matrix[0][i]].index(number_j)] = t[matrix[1][matrix[0][i - 1]:matrix[0][i]].index(number_j)], t[matrix[1][matrix[0][i - 1]:matrix[0][i]].index(number_i)]
+                    matrix[2][matrix[0][i - 1]:matrix[0][i]] = t
+
+                    t = matrix[1][matrix[0][i - 1]:matrix[0][i]]
+                    t[matrix[1][matrix[0][i - 1]:matrix[0][i]].index(number_i)], t[matrix[1][matrix[0][i - 1]:matrix[0][i]].index(number_j)] = t[matrix[1][matrix[0][i - 1]:matrix[0][i]].index(number_j)], t[matrix[1][matrix[0][i - 1]:matrix[0][i]].index(number_i)]
+                    t.sort()
+                    matrix[1][matrix[0][i - 1]:matrix[0][i]] = t
+
+                elif number_i in matrix[1][matrix[0][i - 1]:matrix[0][i]]:
+                    t = matrix[1][matrix[0][i - 1]:matrix[0][i]]
+                    t.insert(bisect.bisect(t, number_j), number_j)
+                    del t[t.index(number_i)]
+                    matrix[1][matrix[0][i - 1]:matrix[0][i]] = t
+
+                elif number_j in matrix[1][matrix[0][i - 1]:matrix[0][i]]:
+                    t = matrix[1][matrix[0][i - 1]:matrix[0][i]]
+                    t.insert(bisect.bisect(t, number_i), number_i)
+                    del t[t.index(number_j)]
+                    matrix[1][matrix[0][i - 1]:matrix[0][i]] = t
+
+        return Matrix(matrix)
+
+    def __pow__(self, power, modulo=None):
+        """
+        Вычисляет обратную матрицу (matrix**(-1))
+        :param power: в какую степень возводим
+        :return: новая матрица
+        """
+        if power == -1:
+            if ~Matrix(self.m) == 0:
+                raise ZeroDivisionError("Обратная матрицы не существует")
+            elif len(self.m[0]) - 1 != self.m[3]:
+                raise IndexError("Матрица не квадратная")
+            matrix = Matrix(copy.deepcopy(self.m)).transposition().m
+            matrix2 = [[0 for _ in range(len(matrix[0]))], [], [], matrix[3]]
+            column = 0
+            for i in range(1, matrix[3] + 1):
+                matrix2[0][i] += matrix2[0][i - 1]
+                for j in range(matrix[3]):
+                    count = ~(Matrix(matrix).delete(i, j + 1)) * (-1) ** (i + j - 1)
+                    if count != 0:
+                        matrix2[0][i] += 1
+                        matrix2[1].append(j)
+                        matrix2[2].append(count)
+            matrix2 = (Matrix(matrix2) * (1 / (~Matrix(matrix))))
+            return matrix2
+
+    def rang(self):
+        """
+        Ранг матрица
+        :return: число
+        """
+        matrix = copy.deepcopy(self.m)
+        if matrix[3] == 1 and len(matrix[2]) <= 1:
+            return 1
+
+        my_min = min(len(matrix[0]) - 1, matrix[3])
+        count = 0
+        for i in range(0, len(matrix[0]) - my_min):
+            count += matrix[0][i]
+            for j in range(0, matrix[3] - my_min + 1):
+                matrix_answ = [matrix[0][i:my_min+1], [], [], my_min]
+                matrix_answ[1] = matrix[1][matrix_answ[0][0]:matrix_answ[0][-1]]
+                matrix_answ[2] = matrix[2][matrix_answ[0][0]:matrix_answ[0][-1]]
+                for k in range(len(matrix_answ[0])):
+                    matrix_answ[0][k] -= count
+                if ~Matrix(matrix_answ) != 0:
+                    return my_min
+
+        m1 = Matrix(matrix_answ).delete(1, 1)
+        m2 = Matrix(matrix_answ).delete(1, matrix[3])
+        m3 = Matrix(matrix_answ).delete(len(matrix[0]) - 1, 1)
+        m4 = Matrix(matrix_answ).delete(len(matrix[0]) - 1, matrix[3])
+
+        return max(m1.rang(), m2.rang(), m3.rang(), m4.rang())
+
+
+m1 = Matrix([[0, 1, 0, 1, 0], [0, 0, 0, 3, 0], [0, 4, 0, 5, 0], [0, 6, 7, 8, 0], [9, 0, 0, 0, 0]])
+m2 = Matrix([[0, 0, 2, 3, 3], [1, 2, 3, 0, 0], [0, 0, 0, 3, 0], [1, 2, 0, 7, 0], [2, 2, 0, 0, 0]])
+m3 = Matrix([[0, 1, 0], [1, 0, 2], [0, 0, 2]])
+m4 = Matrix([[1, -2, 3], [0, 4, -1], [5, 0, 0]])
+m5 = Matrix([[0.5, 0, -1], [1, 0, 2], [2.3, 0, 17]])
+m6 = Matrix([[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]])
+
---
+++ tests/test_sparse_matrix.py
@@ -0,0 +1,122 @@
+from sparse_matrix import Matrix
+import pytest
+
+m1 = Matrix([[0, 1, 0, 1, 0], [0, 0, 0, 3, 0], [0, 4, 0, 5, 0], [0, 6, 7, 8, 0], [9, 0, 0, 0, 0]])
+m2 = Matrix([[0, 0, 2, 3, 3], [1, 2, 3, 0, 0], [0, 0, 0, 3, 0], [1, 2, 0, 7, 0], [2, 2, 0, 0, 0]])
+m3 = Matrix([[0, 1, 0], [1, 0, 2], [0, 0, 2]])
+m4 = Matrix([[1, -2, 3], [0, 4, -1], [5, 0, 0]])
+m5 = Matrix([[0.5, 0, -1], [1, 0, 2], [2.3, 0, 17]])
+m6 = Matrix([[1, 2, 3, 4], [1, 2, 3, 4], [1, 2, 3, 4]])
+
+
+def test_add():
+    assert (m1 + m2).to_matrix() == [[0, 1, 2, 4, 3], [1, 2, 3, 3, 0], [0, 4, 0, 8, 0], [1, 8, 7, 15, 0],
+                                     [11, 2, 0, 0, 0]]
+    assert (m3 + m4).to_matrix() == [[1, -1, 3], [1, 4, 1], [5, 0, 2]]
+    assert (m4 + m5).to_matrix() == [[1.5, -2, 2], [1, 4, 1], [7.3, 0, 17]]
+    with pytest.raises(IndexError):
+        assert m1 + m4
+
+
+def test_sub():
+    assert (m1 - m2).to_matrix() == [[0, 1, -2, -2, -3], [-1, -2, -3, 3, 0], [0, 4, 0, 2, 0], [-1, 4, 7, 1, 0],
+                                     [7, -2, 0, 0, 0]]
+    assert (m3 - m4).to_matrix() == [[-1, 3, -3], [1, -4, 3], [-5, 0, 2]]
+    assert (m4 - m5).to_matrix() == [[0.5, -2, 4], [-1, 4, -3], [2.7, 0, -17]]
+    with pytest.raises(IndexError):
+        assert m1 - m4
+
+
+def test_mul():
+    assert (m1 * m2).to_matrix() == [[2, 4, 3, 7, 0], [3, 6, 0, 21, 0], [9, 18, 12, 35, 0], [14, 28, 18, 77, 0],
+                                     [0, 0, 18, 27, 27]]
+    assert (m3 * m4).to_matrix() == [[0, 4, -1], [11, -2, 3], [10, 0, 0]]
+    assert (m4 * m3).to_matrix() == [[-2, 1, 2], [4, 0, 6], [0, 5, 0]]
+    assert (m3 * 2).to_matrix() == [[0, 2, 0], [2, 0, 4], [0, 0, 4]]
+    with pytest.raises(IndexError):
+        assert m1 * m4
+
+
+def test_slice():
+    assert m1[0] == [0, 1, 0, 1, 0]
+    assert m2[0] == [0, 0, 2, 3, 3]
+    assert m3[0] == [0, 1, 0]
+    assert m4[:][0] == [1, 0, 5]
+    assert m4[:][2] == [3, -1, 0]
+    with pytest.raises(TypeError):
+        assert m1[1.2]
+    with pytest.raises(TypeError):
+        assert m1['a']
+    with pytest.raises(IndexError):
+        assert m1[6]
+
+
+def test_transposition():
+    assert m1.transposition().to_matrix() == [[0, 0, 0, 0, 9], [1, 0, 4, 6, 0], [0, 0, 0, 7, 0], [1, 3, 5, 8, 0],
+                                              [0, 0, 0, 0, 0]]
+    assert m3.transposition().to_matrix() == [[0, 1, 0], [1, 0, 0], [0, 2, 2]]
+    assert m4.transposition().to_matrix() == [[1, 0, 5], [-2, 4, 0], [3, -1, 0]]
+
+
+def test_delete():
+    assert m1.delete(1, 2).to_matrix() == [[0, 0, 3, 0], [0, 0, 5, 0], [0, 7, 8, 0], [9, 0, 0, 0]]
+    assert m3.delete(3, 3).to_matrix() == [[0, 1], [1, 0]]
+    assert m4.delete(3, 1).to_matrix() == [[-2, 3], [4, -1]]
+    with pytest.raises(IndexError):
+        assert m1.delete(1, 6)
+    with pytest.raises(IndexError):
+        assert m3.delete(4, 1)
+
+
+def test_determinant():  # ~
+    assert ~m1 == 0
+    assert ~m2 == -54
+    assert ~m3 == -2
+    with pytest.raises(IndexError):
+        assert ~m6
+
+
+def test_size():
+    assert m1.size() == f"{len(m1.matrix()[0]) - 1} на {m1.matrix()[3]}"
+    assert m2.size() == f"{len(m2.matrix()[0]) - 1} на {m2.matrix()[3]}"
+    assert m3.size() == f"{len(m3.matrix()[0]) - 1} на {m3.matrix()[3]}"
+    assert m4.size() == f"{len(m4.matrix()[0]) - 1} на {m4.matrix()[3]}"
+
+
+def test_permutation():
+    assert m1.permutation("строка", 1, 2).to_matrix() == [[0, 0, 0, 3, 0], [0, 1, 0, 1, 0], [0, 4, 0, 5, 0],
+                                                          [0, 6, 7, 8, 0],
+                                                          [9, 0, 0, 0, 0]]
+    assert m3.permutation("столбец", 1, 3).to_matrix() == [[0, 1, 0], [2, 0, 1], [2, 0, 0]]
+    assert m4.permutation("строка", 1, 2).to_matrix() == [[0, 4, -1], [1, -2, 3], [5, 0, 0]]
+    with pytest.raises(IndexError):
+        assert m1.permutation("Строка", 1, 6)
+    with pytest.raises(IndexError):
+        assert m3.permutation("Строка", 4, 1)
+
+
+def test_reverse():  # **(-1)
+    # assert m1
+    assert (m2 ** (-1)).to_matrix() == [[0, 0, 2.333333333333333, -1.0, 1.0], [0, 0, -2.333333333333333, 1.0, -0.5],
+                                        [0, 0.3333333333333333, 0.7777777777777777, -0.3333333333333333, 0],
+                                        [0, 0, 0.3333333333333333, 0, 0],
+                                        [0.3333333333333333, -0.2222222222222222, -0.8518518518518519,
+                                         0.2222222222222222, 0]]
+    assert (m3 ** (-1)).to_matrix() == [[0, 1.0, -1.0], [1.0, 0, 0], [0, 0, 0.5]]
+    assert (m4 ** (-1)).to_matrix() == [[0, 0, 0.2], [0.1, 0.3, -0.02], [0.4, 0.2, -0.08]]
+    with pytest.raises(ZeroDivisionError):
+        assert m1 ** (-1)
+    with pytest.raises(IndexError):
+        assert m6 ** (-1)
+
+
+def test_rang():
+    assert m1.rang() == 4
+    assert m2.rang() == 5
+    assert m3.rang() == 3
+
+
+def test_to_matrix():
+    assert m1.to_matrix() == [[0, 1, 0, 1, 0], [0, 0, 0, 3, 0], [0, 4, 0, 5, 0], [0, 6, 7, 8, 0], [9, 0, 0, 0, 0]]
+    assert m2.to_matrix() == [[0, 0, 2, 3, 3], [1, 2, 3, 0, 0], [0, 0, 0, 3, 0], [1, 2, 0, 7, 0], [2, 2, 0, 0, 0]]
+    assert m3.to_matrix() == [[0, 1, 0], [1, 0, 2], [0, 0, 2]]
